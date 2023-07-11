8a93e23e4301aeb4dd8eda08ae216514ed20be38
Emre Açar <ea517645@gmail.com>
2023-07-11 16:54:59 +0300

    Site is ready to deploy.

---
+++ src/components/SearchFeed.jsx
@@ -0,0 +1,34 @@
+import { Box, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
+import { Videos } from "./index";
+import { fetchFromAPI } from "../utils/fetchFrom";
+import { useParams } from "react-router-dom";
+
+const SearchFeed = () => {
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) => {
+      setVideos(data.items);
+    });
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: "2 " }}>
+      <Typography
+        variant="h4"
+        fontWeight={"bold"}
+        mb={2}
+        sx={{ color: "white" }}
+      >
+        Search Results for{" "}
+        <span style={{ color: "#f31503" }}>{searchTerm}</span>
+        {videos.length > 1 ? " videos" : " video"}
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
+};
+
+export default SearchFeed;
---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import Video from "./Video";
+
+export { default as Feed } from "./Feed";
+export { default as VideoDetail } from "./VideoDetail";
+export { default as ChannelDetail } from "./ChannelDetail";
+export { default as SearchFeed } from "./SearchFeed";
+export { default as Navbar } from "./Navbar";
+export { default as Videos } from "./Videos";
+export { default as Sidebar } from "./Sidebar";
+export { default as ChannelCard } from "./ChannelCard";
+export { default as VideoCard } from "./VideoCard";
+export { default as Video } from "./Video";
